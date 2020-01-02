3dc1ae74ab5718e6fbb2a964ffea4fc7856a6489
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-02 10:00:41 -0600

    Basic structure of our landing page based on the UX design

---
+++ client/src/components/comingSoon.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
+
+/**
+ * TO-DO - Developer comments
+ */
+export default class comingSoon extends React.Component {
+  render() {
+    return <section className="container">Coming soon section</section>;
+  }
+}
