c26a8114ae398513cceef5037e6bf41fa441f3e0
Mary <mary.clayton1@hotmail.com>
2020-01-02 13:06:55 -0700

    features and benefits update

--- client/src/components/featuresBenefits.js
+++ client/src/components/featuresBenefits.js
@@ -2,8 +2,12 @@ import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
 
 // IMPORTED IMAGES
-import flipit from '../assets/flipit.png'
-
+import flipit from '../assets/flipit.png';
+import glueit1 from '../assets/glueit1.png';
+import glueit2 from '../assets/glueit2.png';
+import dropit1 from '../assets/dropit1.png';
+import dropit2 from '../assets/dropit2.png';
+import enjoyit from '../assets/enjoyit.png';
 /**
  * TO-DO - Developer comments
  */
@@ -11,31 +15,32 @@ export default class featuresBenefits extends React.Component {
   render() {
     return (
       // FEATURES AND BENEFITS SECTION
-    <section className="container section">
-    Features & Benefits
+    <section className="container section center-align">
+    <h2>Features & Benefits</h2>
     {/* ROW 1 */}
     <div className="row">
-      <div className="col l3">
+      <div className="col s12 m7 l5 push-m2">
       {/* CARDS */}
       <div className="card">
-        <img src={flipit} alt="flip it"/>
-        <h4>Flip It</h4>
+        <img src={flipit} alt="flip it" class="img-responsive"/>
+        <h3>Flip It</h3>
       </div>
       <div className="card">
-        <img src={glueit} alt="glue it"/>
-        <h4>Give it</h4>
+        <img src={glueit1} alt="glue it 1" class="img-responsive"/>
+        <img src={glueit2} alt="glue it 2" class="img-responsive"/>
+        <h3>Give it</h3>
       </div>
       </div>
       {/* ROW 2 */}
-      <div className="col l3">
+      <div className="col s12 m7 l5 push-m2 offset-l1">
       <div className="card">
-        <img src={dropit1} alt="drop it 1"/>
-        <img src={dropit2} alt=" drop it 2"/>
-        <h4>Drop It</h4>
+        <img src={dropit1} alt="drop it 1" class="img-responsive"/>
+        <img src={dropit2} alt=" drop it 2" class="img-responsive"/>
+        <h3>Drop It</h3>
       </div>
       <div className="card">
-        <img src={enjoyit} alt="enjoy it"/>
-        <h4>Enjoy It</h4>
+        <img src={enjoyit} alt="enjoy it" class="img-responsive"/>
+        <h3>Enjoy It</h3>
       </div>
     </div>
     </div>
