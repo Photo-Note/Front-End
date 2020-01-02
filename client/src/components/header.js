18feb84bb394c7e7732f38ad685a6ce16dce11f5
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-02 11:22:05 -0600

    Added the assets from UX. Created a nav bar and added the svg logo to it

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
+import "../styles/landing.scss";
+import logo from "../assets/photonote_logo.svg";
 
 /**
  * TO-DO - Developer comments
  */
 export default class header extends React.Component {
+  componentDidMount() {
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelectorAll(".sidenav");
+      var instances = M.Sidenav.init(elems, {});
+    });
+  }
   render() {
-    return <div className="container">Header section</div>;
+    return (
+      <div className="navbar-fixed">
+        <nav className="nav-wrapper header-bg">
+          <div className="container">
+            <img
+              className="responsive-img"
+              src={logo}
+              width="250"
+              height="auto"
+            />
+          </div>
+        </nav>
+      </div>
+    );
   }
 }
