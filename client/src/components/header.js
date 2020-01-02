f05b1e9ad362a53187df512f1d984fe79e533f39
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-02 12:04:51 -0600

    Completed the basic fixed header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -11,7 +11,7 @@ export default class header extends React.Component {
   componentDidMount() {
     document.addEventListener("DOMContentLoaded", function() {
       var elems = document.querySelectorAll(".sidenav");
-      var instances = M.Sidenav.init(elems, {});
+      var instances = M.Sidenav.init(elems, { edge: "right" });
     });
   }
   render() {
@@ -24,9 +24,61 @@ export default class header extends React.Component {
               src={logo}
               width="250"
               height="auto"
+              alt="Photonote app logo"
             />
+            <a
+              href="#test"
+              className="sidenav-trigger right"
+              data-target="mobile-links"
+            >
+              <i className="material-icons turbo-text">menu</i>
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <li className="active">
+                <a href="#test" className="turbo-text nav-text">
+                  How it works
+                </a>
+              </li>
+              <li>
+                <a href="#test" className="turbo-text nav-text">
+                  Features & Benefits
+                </a>
+              </li>
+              <li>
+                <a href="#test" className="turbo-text nav-text">
+                  Team
+                </a>
+              </li>
+              <li>
+                <a href="#test" className="turbo-text nav-text">
+                  My Photos
+                </a>
+              </li>
+            </ul>
           </div>
         </nav>
+        <ul className="sidenav header-bg turbo-text nav-text" id="mobile-links">
+          <li className="active">
+            <a href="#test" className="yellow-text nav-text">
+              How it works
+            </a>
+          </li>
+          <li>
+            <a href="#test" className="yellow-text nav-text">
+              Features and Benefits
+            </a>
+          </li>
+          <li>
+            <a href="#test" className="yellow-text nav-text">
+              Team
+            </a>
+          </li>
+          <li>
+            <a href="www.google.com" className="yellow-text nav-text">
+              My Photos
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
