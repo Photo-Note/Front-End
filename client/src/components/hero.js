d1e1dbd30bba5fe31a9ecf3bd3e5cd095ca4bcd9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-02 14:07:42 -0600

    Hero-section: Adding the right content

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -4,6 +4,7 @@ import HeaderSection from "./header.js";
 import M from "materialize-css";
 import Carousel1 from "../assets/Carousel 1.png";
 import "../styles/landing.scss";
+import HeroAppStore from "../assets/heroAppStore.png";
 
 /**
  * TO-DO - Developer comments
@@ -19,8 +20,8 @@ export default class hero extends React.Component {
     return (
       <header>
         <HeaderSection />
-        <div className="container">
-          <div className="carousel valign-wrapper">
+        <div className="container row">
+          <div className="carousel valign-wrapper col s6 m8 l8">
             <a className="carousel-item" href="#one!">
               <img src={Carousel1} className="carousel-img" />
             </a>
@@ -33,6 +34,12 @@ export default class hero extends React.Component {
             <a className="carousel-item" href="#one!">
               <img src={Carousel1} className="carousel-img" />
             </a>
+          </div>
+          <div className="col s6 m4 l4 valign-wrapper hero-right">
+            <div className="hero-right-content">
+              <span>Try it Now!</span>
+              <img src={HeroAppStore} alt="" />
+            </div>
           </div>
         </div>
       </header>
