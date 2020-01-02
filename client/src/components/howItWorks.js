81cc440733752f5d5bb7ea33814696f29feaf793
Vicky <vyue013@gmail.com>
2020-01-02 12:09:41 -0800

    added title and rough sections

--- client/src/components/howItWorks.js
+++ client/src/components/howItWorks.js
@@ -6,6 +6,21 @@ import "materialize-css/dist/css/materialize.min.css";
  */
 export default class howItWorks extends React.Component {
   render() {
-    return <section className="container">How it works section</section>;
+    return (
+      <div>
+        <section className="container">
+          <div className="how-title">How It Works</div>
+          <div className="box-section">
+            <div class="box-attr box-left">
+              Drop. Flip. Write.
+            </div>
+            <div className="box-attr box-right">
+              Save & Share
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+    // return <section className="container">How it works section</section>;
   }
 }
