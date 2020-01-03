6bcde53a8a629aecc1bbe26928cef35ffc5dc2f1
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-03 10:27:06 -0600

    Merge-footer: Updated to previous version

--- client/src/components/comingSoon.js
+++ client/src/components/comingSoon.js
@@ -11,24 +11,28 @@ export default class ComingSoon extends React.Component {
   render() {
     return (
       //SECTION
-    <section className="container center-align">
-      {/* UNORDERED LIST */}
-      <h2>Coming Soon</h2>
-      <ul className="comingsoon">
-        <li>Filter</li>
-        <li>Frames</li>
-        <li>Secret Messages</li>
-        <li>Social Sharing</li>
-        <li>Scrapbooks/Photo Journals</li>
-        <li>And MORE!</li>
-      </ul>
-      {/* IMAGE */}
-      <img src={app} alt="app store" className="img-responsive"/>
-      <div className="row">
-        {/* TEXT */}
-      <a href="#" id="apptext">Get a Text!</a>
+      <div className="comingsoon-section">
+        <section className="container center-align ">
+          <h2 className="gutter-top">Coming Soon!</h2>
+          {/* UNORDERED LIST */}
+          <ul className="comingsoon">
+            <li>Filter</li>
+            <li>Frames</li>
+            <li>Secret Messages</li>
+            <li>Social Sharing</li>
+            <li>Scrapbooks/Photo Journals</li>
+            <li>And MORE!</li>
+          </ul>
+          {/* IMAGE */}
+          <img src={app} alt="app store" class="img-responsive" />
+          <div class="row">
+            {/* TEXT */}
+            <a href="#" id="apptext">
+              Get a Text!
+            </a>
+          </div>
+        </section>
       </div>
-      </section>
     );
   }
 }
