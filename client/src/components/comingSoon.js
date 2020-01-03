31994ac5c0972abab35fdec69bff17d5c366d267
Mary <mary.clayton1@hotmail.com>
2020-01-02 20:30:12 -0700

    footer in progress

--- client/src/components/comingSoon.js
+++ client/src/components/comingSoon.js
@@ -13,6 +13,7 @@ export default class ComingSoon extends React.Component {
       //SECTION
     <section className="container center-align">
       {/* UNORDERED LIST */}
+      <h2>Coming Soon</h2>
       <ul className="comingsoon">
         <li>Filter</li>
         <li>Frames</li>
@@ -22,8 +23,8 @@ export default class ComingSoon extends React.Component {
         <li>And MORE!</li>
       </ul>
       {/* IMAGE */}
-      <img src={app} alt="app store" class="img-responsive"/>
-      <div class="row">
+      <img src={app} alt="app store" className="img-responsive"/>
+      <div className="row">
         {/* TEXT */}
       <a href="#" id="apptext">Get a Text!</a>
       </div>
--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,11 +1,34 @@
 import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
 
+//IMPORTED IMAGES
+import logo from '../assets/photonote_logo.svg';
+import facebook from '../assets/facebook.svg';
+import twitter from '../assets/twitter.svg';
+import instagram from '../assets/instagram.svg';
 /**
  * TO-DO - Developer comments
  */
 export default class footer extends React.Component {
   render() {
-    return <div className="container">Footer section</div>;
+    return (
+    <div className="footer">
+      <div id="footertop">
+      <img src={logo} id="logo" alt="logo"/>
+      <div id="socialmedia">
+      <img src={facebook}class="social" alt="facebook"/>
+      <img src={twitter} class="social"alt="twitter"/>
+      <img src={instagram} class="social"alt="instagram"/>
+      </div>
+      </div>
+      <div id="footerbottom">
+        <form class="center-align">
+          <input type="text" name="name"></input>
+          <input type="email" name="email"></input>
+          <button class="btn"></button>
+        </form>
+      </div>
+      </div>
+    );
   }
 }
