fe804fdeb3efa4cca2fdb055bf36bbeeafa6e806
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-03 15:31:10 -0600

    Updated the features and benefits with the latest phone screens

--- client/src/components/featuresBenefits.js
+++ client/src/components/featuresBenefits.js
@@ -2,16 +2,25 @@ import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
 
 // IMPORTED IMAGES
-import flipit from "../assets/flipit.png";
-import glueit1 from "../assets/glueit1.png";
-import glueit2 from "../assets/glueit2.png";
-import dropit1 from "../assets/dropit1.png";
-import dropit2 from "../assets/dropit2.png";
-import enjoyit from "../assets/enjoyit.png";
+import FeaturesFlipBackImage from "../assets/Features_Flip_Back_Image.png";
+import FeaturesFlipBackText from "../assets/Features_Flip_Back_Text.png";
+import FeaturesFlipFrontImage from "../assets/Features_Flip_Front_Image.png";
+import FeaturesFlipTextFront from "../assets/Features_Flip_text_Front.png";
+import FeaturesDrop1 from "../assets/Features_Drop_1.png";
+import FeaturesDrop2 from "../assets/Features_Drop_2.png";
+import FeaturesGallery from "../assets/Features_Gallery.png";
+import M from "materialize-css";
+
 /**
  * TO-DO - Developer comments
  */
 export default class featuresBenefits extends React.Component {
+  componentDidMount() {
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelectorAll(".materialboxed");
+      var instances = M.Materialbox.init(elems, {});
+    });
+  }
   render() {
     return (
       // FEATURES AND BENEFITS SECTION
@@ -28,19 +37,28 @@ export default class featuresBenefits extends React.Component {
           <div className="col s12 m7 l5 push-m2">
             {/* CARDS */}
             <div className="card card-update">
-              <img src={flipit} alt="flip it" class="img-responsive phones" />
+              <img
+                src={FeaturesFlipBackImage}
+                alt="flip it"
+                class="responsive-img phones materialboxed"
+              />
+              <img
+                src={FeaturesFlipBackText}
+                alt="flip it"
+                class="responsive-img phones materialboxed"
+              />
               <h3>Flip It</h3>
             </div>
             <div className="card card-update">
               <img
-                src={glueit1}
+                src={FeaturesFlipFrontImage}
                 alt="glue it 1"
-                class="img-responsive phones"
+                class="responsive-img phones materialboxed"
               />
               <img
-                src={glueit2}
+                src={FeaturesFlipTextFront}
                 alt="glue it 2"
-                class="img-responsive phones"
+                class="responsive-img phones materialboxed"
               />
               <h3>Glue it</h3>
             </div>
@@ -48,20 +66,26 @@ export default class featuresBenefits extends React.Component {
           {/* ROW 2 */}
           <div className="col s12 m7 l5 push-m2 offset-l1">
             <div className="card card-update">
+              <div>
+                <img
+                  src={FeaturesDrop1}
+                  alt="drop it 1"
+                  class="responsive-img phones materialboxed"
+                />
+              </div>
               <img
-                src={dropit1}
-                alt="drop it 1"
-                class="img-responsive phones"
-              />
-              <img
-                src={dropit2}
+                src={FeaturesDrop2}
                 alt=" drop it 2"
-                class="img-responsive phones"
+                class="responsive-img phones materialboxed"
               />
               <h3>Drop It</h3>
             </div>
             <div className="card card-update">
-              <img src={enjoyit} alt="enjoy it" class="img-responsive phones" />
+              <img
+                src={FeaturesGallery}
+                alt="enjoy it"
+                class="responsive-img phones materialboxed"
+              />
               <h3>Enjoy It</h3>
             </div>
           </div>
