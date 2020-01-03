ddb61f056b243946085e6d1f4436f64a7e84f7ae
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-02 18:52:57 -0600

    firebase-config: Set up and tested the connection with firestore. A sample data is printed to the screen

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,16 +1,38 @@
 import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import Header from "../components/header.js";
+import { getPhotoNote } from "../actions/index.js";
+import { connect } from "react-redux";
+import Card from "../components/card.js";
 
 /**
  * TO-DO - Developer comments
  */
-export default class Landing extends React.Component {
+class Dashboard extends React.Component {
+  state = {
+    imageURL: "",
+    message: ""
+  };
+
+  componentDidMount() {
+    this.props.getPhotoNote(this.state);
+  }
   render() {
     return (
       <div>
         <Header />
+        <Card imageVal={this.props.imageURL} />
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  console.log("++++++++++++++++++++", state.imageURL);
+  return {
+    imageURL: state.imageURL,
+    message: state.message
+  };
+};
+
+export default connect(mapStateToProps, { getPhotoNote })(Dashboard);
